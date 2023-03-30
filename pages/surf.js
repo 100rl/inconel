91547d8116152240c83b11b07cd912de57a80cb5
100rl <124484905+100rl@users.noreply.github.com>
2023-03-29 22:24:02 -0700

    Update surf.js

--- pages/surf.js
+++ pages/surf.js
@@ -17,7 +17,8 @@ export default function Surf() {
         }
 
         e.target.reset();
-        window.location.href = `https://www.nebulaofficial.gq/proxy/${btoa(browse.toString())}`;
+        router.push(`/browse/${browse.toString()}`);
+
     }
 
     return (
@@ -26,5 +27,8 @@ export default function Surf() {
             <input className="input w-7/12 h-10" placeholder="https://www.google.com" onChange={e => setURL(e.target.value)} required />
             <button className="text-4xl font-bold text-center border-none border-2 hover:text-purple-400" type="submit">&#8594;</button>
         </form>
+
     )
+    npm run build
+    npm start
 }
